f2d9b2cc70d27fd0e4ba0ed904a9e03ed1946e65
afreen009 <shfafreen@gmail.com>
2024-08-02 20:20:53 -0400

    did initial clean architecture setup

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff1A3636);
+  static const primary1 = Color(0xff40534C);
+  static const secondary = Color(0xff677D6A);
+  static const secondary1 = Color(0xffD6BD98);
+  static const textColor = Color(0xffF7F7F7);
+  static const darkGrey = Color(0xff343434);
+}
